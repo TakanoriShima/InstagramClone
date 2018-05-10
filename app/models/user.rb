44caf72d312f583445ac3fa68436b3287016a7eb
Tomoyuki Kurosawa <1717271+7coAim@users.noreply.github.com>
2018-05-10 21:33:08 +0900

    Edit models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  validates :name,  presence: true, length: { in: 3..32 }
+  validates :email, presence: true, length: { in: 5..256 }, uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_save { email.downcase! }
+  has_secure_password
+  validates :password, presence: true, length: { in: 6..32 }
+  has_many :pictures, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_pictures, through: :favorites, source: :picture
+  mount_uploader :image, ImageUploader
 end
