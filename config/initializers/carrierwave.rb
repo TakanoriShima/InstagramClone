f14f0645a8fae7f740719dadcf3a5cb7892037fe
Tomoyuki Kurosawa <1717271+7coAim@users.noreply.github.com>
2018-06-11 19:24:47 +0900

    change ENV arguments name

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_REGION']
+    aws_access_key_id: ENV['S3_ACCESS_KEY'],
+    aws_secret_access_key: ENV['S3_SECRET_KEY'],
+    region: ENV['S3_REGION']
   }
 
-  config.fog_directory  = ENV['CONFIG_FOG_DIRECTORY']
+  config.fog_directory  = ENV['S3_BUCKET']
   config.cache_storage = :fog
 end
