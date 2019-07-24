91fa85877eee26ae17aa2e6558bc07157c687cee
EC2 Default User <ec2-user@ip-172-31-36-92.us-east-2.compute.internal>
2019-07-24 11:36:46 +0000

    hoge

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,8 @@ require 'carrierwave/storage/fog'
 if Rails.env.development?                                                                                                                   
   CarrierWave.configure do |config|
     # developmentのもろもろの設定
-    config.cache_storage = :fog
-    config.storage = :fog
+    config.cache_storage = :file
+    config.storage = :file
   end 
 elsif Rails.env.test?
   CarrierWave.configure do |config|
@@ -17,8 +17,17 @@ elsif Rails.env.test?
   end 
 else
   CarrierWave.configure do |config|
-    # productionのもろもろの設定
-    config.cache_storage = :fog # fogを使う環境だけに適用！
     config.storage = :fog
-  end 
+    config.cache_storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+        :provider              => 'AWS',
+        :aws_access_key_id     => ENV['S3_ACCESS_KEY_ID'],
+        :aws_secret_access_key => ENV['S3_SECRET_KEY_ID'],
+        :region                => ENV['S3_REGION'],
+    }
+    
+    config.fog_directory = ENV['S3_BUCKET_NAME']
+    config.fog_public = true
+  end
 end
