16e1f473146201a1720c75bc2959554cc805d123
EC2 Default User <ec2-user@ip-172-31-36-92.us-east-2.compute.internal>
2019-07-24 05:55:35 +0000

    GridMail以外はローカルで正常に動く

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,16 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['S3_ACCESS_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET_KEY'],
-    region: ENV['S3_REGION']
-  }
-
-  config.fog_directory  = ENV['S3_BUCKET']
-  config.cache_storage = :fog
+if Rails.env.development?                                                                                                                   
+  CarrierWave.configure do |config|
+    # developmentのもろもろの設定
+    config.cache_storage = :file
+  end 
+elsif Rails.env.test?
+  CarrierWave.configure do |config|
+    # testのもろもろの設定
+    config.cache_storage = :file
+  end 
+else
+  CarrierWave.configure do |config|
+    # productionのもろもろの設定
+    config.cache_storage = :fog # fogを使う環境だけに適用！
+  end 
 end
