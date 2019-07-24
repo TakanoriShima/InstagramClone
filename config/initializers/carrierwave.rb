4ca0290dce953047885c6e3a1d8eb23d3f78b4f1
EC2 Default User <ec2-user@ip-172-31-36-92.us-east-2.compute.internal>
2019-07-24 11:09:33 +0000

    上手くいく？

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,17 +2,18 @@ if Rails.env.development?
   CarrierWave.configure do |config|
     # developmentのもろもろの設定
     config.cache_storage = :file
-    storage :file
+    config.storage = :file
   end 
 elsif Rails.env.test?
   CarrierWave.configure do |config|
     # testのもろもろの設定
     config.cache_storage = :file
+    config.storage = :file
   end 
 else
   CarrierWave.configure do |config|
     # productionのもろもろの設定
     config.cache_storage = :fog # fogを使う環境だけに適用！
-    storage :file
+    config.storage = :fog
   end 
 end
