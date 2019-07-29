d66c5ceddea55b2beb333893625b0da37253079b
EC2 Default User <ec2-user@ip-172-31-38-86.ap-northeast-1.compute.internal>
2019-07-29 11:17:40 +0000

    Cloud9からS3に画像アップ成功

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,54 +1,14 @@
-#require 'carrierwave/storage/abstract'
-#require 'carrierwave/storage/file'
-#require 'carrierwave/storage/fog'
-#require 'aws-sdk'
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
-if Rails.env.development?                                                                                                              
-  CarrierWave.configure do |config|
-    # developmentのもろもろの設定
-    #config.cache_storage = :file
-    #config.storage = :file
-    #config.cache_storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-        :provider              => 'AWS',
-        :region                => ENV['S3_REGION'],
-        #:s3_access_key_id     => ENV['S3_ACCESS_KEY_ID'],
-        #:s3_secret_access_key => ENV['S3_SECRET_KEY_ID'],
-        #:region                => ENV['S3_REGION'],
-        :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-        :aws_secret_access_key => ENV['AWS_SECRET_ACESSS_KEY']
-    }
-    #config.storage = :fog
-    
-    config.fog_directory = ENV['S3_BUCKET']
-    #config.fog_public = true
-  end 
-elsif Rails.env.test?
-  CarrierWave.configure do |config|
-    # testのもろもろの設定
-    config.cache_storage = :file
-    config.storage = :file
-  end 
-else
-  CarrierWave.configure do |config|
-    #config.fog_provider = 'fog/aws'
-    #config.storage = :fog
-    #config.cache_storage = :fog
-    #config.fog_credentials = {
-    #    :provider              => 'AWS',
-    #    :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-    #    :aws_secret_access_key => ENV['AWS_SECRET_ACEESS_KEY'],
-    #    :region                => ENV['S3_REGION']
-    #    #:aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-    #    #:aws_secret_access_key => ENV['AWS_SECRET_KEY_ID'],
-    #    #:region                => ENV['S3_REGION'],
-    #}
-    
-    #config.fog_directory = ENV['S3_BUCKET']
-    #config.fog_public = true
-  end
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV['AWS_S3_ACCESS_KEY_ID'],  # required
+    aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],     # required
+    region:                'ap-northeast-1',                   # optional, defaults to 'us-east-1' オハイオ＝us-east-2
+    #host:                  's3.example.com',             # optional, defaults to nil
+    #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'quark2galaxy'                # required
+  config.fog_public     = false                                                 # optional, defaults to true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
