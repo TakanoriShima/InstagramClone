81f46c1c4e64d6a0c1b3b2a06205f1d34565be50
EC2 Default User <ec2-user@ip-172-31-42-63.us-east-2.compute.internal>
2019-07-31 02:12:32 +0000

    development環境で画像をS3に保存成功

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,11 +4,11 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV['AWS_S3_ACCESS_KEY_ID'],  # required
     aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],     # required
-    region:                'ap-northeast-1',                   # optional, defaults to 'us-east-1' オハイオ＝us-east-2
+    region:                ENV['AWS_REGION']                  # optional, defaults to 'us-east-1' オハイオ＝us-east-2
     #host:                  's3.example.com',             # optional, defaults to nil
     #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = 'quark2galaxy'                # required
+  config.fog_directory  = ENV['AWS_S3_BUCKET']                # required
   config.fog_public     = false                                                 # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
